bb558e0ca5f8969f549c1350cb1de39656a90eda
Shao-Heng <95068443+paulchen2713@users.noreply.github.com>
2022-05-22 12:34:46 +0800

    Update matrix_multiplication_power.m

--- matrix_multiplication_power.m
+++ matrix_multiplication_power.m
@@ -1,22 +1,24 @@
 %
 % matrix multiplication over power representation
 %
-function out=matrix_multiplication_power(A,B)
-global m;
-zero=2^m;
-Ax=size(A,1);
-By=size(B,2);
-kk=size(A,2);
-AB=zeros(Ax,By);
-for ir=1:Ax
-    for ic=1:By
-        sum=zero;
-        for ik=1:kk
-            ab=multiplication_power(A(ir,ik),B(ik,ic));
-            sum=addition_power(sum,ab);
+function out = matrix_multiplication_power(A, B)
+    global m;
+    zero = 2^m;
+    Ax = size(A, 1);
+    By = size(B, 2);
+    kk = size(A, 2);
+    AB = zeros(Ax, By);
+    for ir = 1:Ax
+        for ic = 1:By
+            sum = zero;
+            for ik = 1:kk
+                ab  = multiplication_power(A(ir, ik), B(ik, ic));
+                sum = addition_power(sum, ab);
+            end
+            AB(ir, ic) = sum;
         end
-        AB(ir,ic)=sum;
     end
-end
-out=AB;
+    out = AB;
 return
+
+
