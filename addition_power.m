e47675877cc03bfa78fe6707e8ab56d8ad933a0a
Shao-Heng <95068443+paulchen2713@users.noreply.github.com>
2022-05-22 11:56:00 +0800

    Update addition_power.m

--- addition_power.m
+++ addition_power.m
@@ -4,6 +4,6 @@
 function n = addition_power(n1, n2)
   global alpha alpha1;
   nc = bitxor(alpha(n1, :), alpha(n2, :));
-  n = alpha1(bin2dec(char(nc + 48)) + 1);
+  n  = alpha1(bin2dec(char(nc + 48)) + 1);
 return
 
