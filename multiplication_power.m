8f96eb3a4620c6d9e8548dba33126091a1c43bcf
Shao-Heng <95068443+paulchen2713@users.noreply.github.com>
2022-05-25 14:41:21 +0800

    Update multiplication_power.m

--- multiplication_power.m
+++ multiplication_power.m
@@ -2,19 +2,19 @@
 % multiplication of numbers with power representation
 %
 function n = multiplication_power(n1, n2)
-global m;
-two_m=2^m;
-two_m_1=two_m-1;
-one=two_m_1;
-zero=two_m;
-if n1==zero || n2==zero
-    n=zero;
-else
-    n=mod(n1+n2,two_m_1);
-    if n==0
-        n=one;
+    global m;
+    two_m   = 2^m;
+    two_m_1 = two_m - 1;
+    one  = two_m_1;
+    zero = two_m;
+    if n1 == zero || n2 == zero
+        n = zero;
+    else
+        n = mod(n1 + n2, two_m_1);
+        if n == 0
+            n = one;
+        end
     end
-end
 return
 
 
