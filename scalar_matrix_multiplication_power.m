64c27b65791f42a1aa11f188cebd7de372afc7d1
Shao-Heng <95068443+paulchen2713@users.noreply.github.com>
2022-05-31 11:30:53 +0800

    Update scalar_matrix_multiplication_power.m

--- scalar_matrix_multiplication_power.m
+++ scalar_matrix_multiplication_power.m
@@ -1,13 +1,14 @@
 %
 % matrix multiplication over power representation
 %
-function out=scalar_matrix_multiplication_power(a,B)
-Bx=size(B,1);
-By=size(B,2);
-for ir=1:Bx
-    for ic=1:By
-        B(ir,ic)=multiplication_power(a,B(ir,ic));
+function out = scalar_matrix_multiplication_power(a, B)
+    Bx = size(B, 1);
+    By = size(B, 2);
+    for ir = 1:Bx
+        for ic = 1:By
+            B(ir, ic) = multiplication_power(a, B(ir, ic));
+        end
     end
-end
-out=B;
+    out = B;
 return
+
