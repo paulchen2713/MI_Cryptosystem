f83811206a78d138134ad16ad024dc66c1141266
Shao-Heng <95068443+paulchen2713@users.noreply.github.com>
2022-06-01 11:56:58 +0800

    Update addition_power.m

--- addition_power.m
+++ addition_power.m
@@ -2,8 +2,8 @@
 % addition of numbers with power representation
 %
 function n = addition_power(n1, n2)
-  global alpha alpha1;
-  nc = bitxor(alpha(n1, :), alpha(n2, :));
-  n  = alpha1(bin2dec(char(nc + 48)) + 1);
+    global alpha alpha1;
+    nc = bitxor(alpha(n1, :), alpha(n2, :));
+    n  = alpha1(bin2dec(char(nc + 48)) + 1);
 return
 
